2d4ede7d03534b09889023f76f3ae617d19807ea
srinibas <srinibaskhuntia39@gmail.com>
2022-07-08 20:44:32 +0530

    added styling to todo app

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.todoContainer}>
+        <TextInput placeholder="ENTER YOUR TODO HERE" style={styles.todoInput} />
+        <Button title='ADD'/>
+      </View>
+      <View style={styles.todoList}>
+        <Text>All todos here....</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    marginVertical:32,
+    flex:1,
   },
+  todoContainer: {
+    flex:1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems:"center",
+    borderBottomWidth:2,
+    borderBottomColor:"#0ea5e9",
+    backgroundColor:"#e0f2fe",
+    padding:16,
+   
+  },
+  todoInput: {
+    borderWidth: 2,
+    borderColor:"#0ea5e9",
+    width: "80%",
+    paddingLeft: 10,
+    paddingRight: 10,
+    height:38,
+    backgroundColor:"#fff"
+  
+  },
+  todoList:{
+     flex:6,
+     backgroundColor:"#e0f2fe",
+     
+  }
+ 
 });
