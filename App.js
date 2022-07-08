48a3bdf01972e7c358ef0a221a33cacd7244d522
srinibas <srinibaskhuntia39@gmail.com>
2022-07-08 21:18:21 +0530

    mapped elements

--- App.js
+++ App.js
@@ -1,16 +1,36 @@
 
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 
 export default function App() {
+  const [todo, setTodo] = useState([]);
+  const [text, setText] = useState("")
+
+  const handleChange = (enteredText) => {
+    setText(enteredText);
+   
+   
+
+  }
+  const handlePress = () => {
+    setTodo((ptodo) => [...ptodo, text])
+    setText("");
+  
+  }
   return (
     <View style={styles.appContainer}>
       <View style={styles.todoContainer}>
-        <TextInput placeholder="ENTER YOUR TODO HERE" style={styles.todoInput} />
-        <Button title='ADD'/>
+        <TextInput placeholder="ENTER YOUR TODO HERE" value={text}  style={styles.todoInput} onChangeText={handleChange} />
+        <Button title='ADD' onPress={handlePress} />
       </View>
       <View style={styles.todoList}>
-        <Text>All todos here....</Text>
+        {
+          todo.map((el,idx)=>{
+            return <Text key={idx}>{el}</Text>
+          })
+        }
+        
       </View>
     </View>
   );
@@ -18,34 +38,31 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    marginVertical:32,
-    flex:1,
+    marginVertical: 32,
+    flex: 1,
   },
   todoContainer: {
-    flex:1,
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems:"center",
-    borderBottomWidth:2,
-    borderBottomColor:"#0ea5e9",
-    backgroundColor:"#e0f2fe",
-    padding:16,
-   
+    alignItems: "center",
+    borderBottomWidth: 2,
+    borderBottomColor: "#0ea5e9",
+    padding: 16,
+
   },
   todoInput: {
     borderWidth: 2,
-    borderColor:"#0ea5e9",
+    borderColor: "#0ea5e9",
     width: "80%",
     paddingLeft: 10,
     paddingRight: 10,
-    height:38,
-    backgroundColor:"#fff"
-  
+    height: 38,
+    backgroundColor: "#fff"
+
   },
-  todoList:{
-     flex:6,
-     backgroundColor:"#e0f2fe",
-     
+  todoList: {
+    flex: 6,
   }
- 
+
 });
