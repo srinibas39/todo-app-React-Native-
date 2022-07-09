8783c0bab51aef2118f81efbf4299c948fa543cb
srinibas <srinibaskhuntia39@gmail.com>
2022-07-09 14:03:48 +0530

    added Modal

--- App.js
+++ App.js
@@ -1,30 +1,52 @@
 
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, View, Button, TextInput, Modal } from 'react-native';
 import { TodoList } from './components/todoList';
 
 
 
 export default function App() {
   const [todo, setTodo] = useState([]);
-  const [text, setText] = useState("")
+  const [text, setText] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+
+
 
   const handleChange = (enteredText) => {
     setText(enteredText);
   }
   const handlePress = () => {
-    setTodo((ptodo) => [...ptodo, { text: text, id: Math.random().toString()}])
+    setTodo((ptodo) => [...ptodo, { text: text, id: Math.random().toString() }])
     setText("");
+    handleCloseModal();
   }
+  const handleModal = () => {
+    setModalVisible(true)
+  }
+
+  const handleCloseModal = () => {
+    setModalVisible(false)
+  }
+
 
   return (
     <View style={styles.appContainer}>
-      
+      <Button title='OPEN TODO EDITOR' onPress={handleModal} />
+      <Modal visible={modalVisible} animationType="slide">
         <View style={styles.todoContainer}>
           <TextInput placeholder="ENTER YOUR TODO HERE" value={text} style={styles.todoInput} onChangeText={handleChange} />
-          <Button title='ADD' onPress={handlePress} />
+          <View style={styles.BtnContainer}>
+            <View>
+              <Button title='ADD' onPress={handlePress} />
+            </View>
+            <View>
+              <Button title='CANCEL' onPress={handleCloseModal} />
+            </View>
+          </View>
+
         </View>
-    
+      </Modal>
+
       <TodoList todo={todo} setTodo={setTodo} />
     </View>
   );
@@ -34,28 +56,35 @@ const styles = StyleSheet.create({
   appContainer: {
     marginVertical: 32,
     flex: 1,
+    padding: 16
   },
   todoContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: "column",
+    justifyContent: "center",
     alignItems: "center",
     borderBottomWidth: 2,
     borderBottomColor: "#0ea5e9",
-
     padding: 16,
 
   },
   todoInput: {
     borderWidth: 2,
     borderColor: "#0ea5e9",
-    width: "80%",
+    width: "100%",
     paddingLeft: 10,
     paddingRight: 10,
     height: 38,
     backgroundColor: "#fff"
 
   },
+  BtnContainer: {
+    margin: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "40%"
+  }
+
 
 
 });
